405191a2aa1e620307ad1d185e40b5ea6423c2a7
Boomrock <63188345+Boomrock@users.noreply.github.com>
2023-04-15 21:37:34 +0700

    Initial commit

---
+++ Assets/Script/MoveController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private float horisontalInput, verticalInput;
+    private Rigidbody2D rigidbody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horisontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(horisontalInput * speed , verticalInput * speed);
+        if (horisontalInput > 0)
+        {
+            Quaternion rot = transform.rotation;
+            rot.y = 0;
+            transform.rotation = rot;
+        }
+
+        if (horisontalInput < 0)
+        {
+            Quaternion rot = transform.rotation;
+            rot.y = 180;
+            transform.rotation = rot;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Air_Supply
+ 
