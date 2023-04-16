75b056f515c3c90f2cdccfe400c93cf921253fb6
Boomrock <63188345+Boomrock@users.noreply.github.com>
2023-04-16 12:07:12 +0700

    Анимация двери

---
+++ Assets/Script/Animation Controller/DoorOpeningScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpeningScript : MonoBehaviour
+{
+    public BoxCollider2D boxCollider;
+    public Animator DoorAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player"){  
+            DoorAnimator.SetBool("isDoorOpen", false);
+        }
+    }
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player"){
+            DoorAnimator.SetBool("isDoorOpen", true);
+        }
+    }
+}
