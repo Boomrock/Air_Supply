b687a7393c1917f090c0601db02e5b7304779b06
Boomrock <63188345+Boomrock@users.noreply.github.com>
2023-04-16 12:14:46 +0700

    движение камеры

---
+++ Assets/Script/Player Sripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float Speed;
+    public GameObject Player;
+    public GameObject Camera;
+
+    void FixedUpdate()
+    {
+        Vector3 target = new Vector3()
+        {
+            x = Player.transform.position.x,
+            y = Player.transform.position.y,
+            z = -10,
+        };
+        Camera.transform.position = Vector3.Lerp(Camera.transform.position, target, Speed * Time.fixedDeltaTime);
+    }
+}
